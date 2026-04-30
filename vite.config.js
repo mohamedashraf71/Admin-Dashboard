42412f2ad076d1c9a5ebd643d1f80704a999e89f
Mohamed Ashraf <mohamedashrafh300@gmail.com>
2026-04-30 15:16:59 +0300

    Deploy with gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-base: './',
+  base: '/Admin-Dashboard/',
   server: {
     port: 5173,
     host: true
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
